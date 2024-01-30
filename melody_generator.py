7854d14f5e19068293593dfd8f17097252ccc2ab
Jerome Gutierrez <jerome.gutierrez@lexmeet.com>
2024-01-30 12:09:31 +0800

    feat: error handling

--- melody_generator.py
+++ melody_generator.py
@@ -28,34 +28,37 @@ class MelodyGenerator:
         # Map seed to int
         seed = [self._mappings[symbol] for symbol in seed]
 
-        for _ in range(num_steps):
-            # Limit the seed to max_sequence_length
-            seed = seed[-max_sequence_length:]
+        try:
+            for _ in range(num_steps):
+                # Limit the seed to max_sequence_length
+                seed = seed[-max_sequence_length:]
 
-            # One-hot encode the seed
-            onehot_seed = keras.utils.to_categorical(seed, num_classes=len(self._mappings))
+                # One-hot encode the seed
+                onehot_seed = keras.utils.to_categorical(seed, num_classes=len(self._mappings))
 
-            # (1, max_sequence_length, num of symbols in the vocabulary)
-            onehot_seed = onehot_seed[np.newaxis, ...]
+                # (1, max_sequence_length, num of symbols in the vocabulary)
+                onehot_seed = onehot_seed[np.newaxis, ...]
 
-            # Max prediction
-            probabilities = self.model.predict(onehot_seed)[0]
+                # Max prediction
+                probabilities = self.model.predict(onehot_seed)[0]
 
-            # [0.1, 0.2, 0.1, 0.6] -> 1
-            output_int = self._sample_with_temperature(probabilities, temperature)
+                # [0.1, 0.2, 0.1, 0.6] -> 1
+                output_int = self._sample_with_temperature(probabilities, temperature)
 
-            # Update seed
-            seed.append(output_int)
+                # Update seed
+                seed.append(output_int)
 
-            # Map int to our encoding
-            output_symbol = [key for key, value in self._mappings.items() if value == output_int][0]
+                # Map int to our encoding
+                output_symbol = [key for key, value in self._mappings.items() if value == output_int][0]
 
-            # Check the end of a melody
-            if output_symbol == "/":
-                break
+                # Check the end of a melody
+                if output_symbol == "/":
+                    break
 
-            # Update the melody
-            melody.append(output_symbol)
+                # Update the melody
+                melody.append(output_symbol)
+        except Exception as e:
+            print(f'Exception raised: {e}')
 
         return melody
 
@@ -63,11 +66,14 @@ class MelodyGenerator:
         # temperature -> infinity
         # temperature -> 0
         # temperature = 1
-        predictions = np.log(probabilities) / temperature
-        probabilities = np.exp(predictions) / np.sum(np.exp(predictions))
+        try:
+            predictions = np.log(probabilities) / temperature
+            probabilities = np.exp(predictions) / np.sum(np.exp(predictions))
 
-        choices = range(len(probabilities))  # [0, 1, 2, 3]
-        index = np.random.choice(choices, p=probabilities)
+            choices = range(len(probabilities))  # [0, 1, 2, 3]
+            index = np.random.choice(choices, p=probabilities)
+        except Exception as e:
+            print(f'Exception raised: {e}')
 
         return index
 
@@ -79,27 +85,30 @@ class MelodyGenerator:
         start_symbol = None
         step_counter = 1
 
-        for i, symbol in enumerate(melody):
-            # Handle case in which we have a note/rest
-            if symbol != "_" or i + 1 == len(melody):
-                # Ensure we're dealing with note/rest beyond the first note/rest
-                if start_symbol is not None:
-                    quarter_length_duration = step_duration * step_counter  # 0.25 * 4 = 1
-                    # Handle rest
-                    if start_symbol == "r":
-                        m21_event = m21.note.Rest(quarterLength=quarter_length_duration)
-                    # Handle note
-                    else:
-                        m21_event = m21.note.Note(int(start_symbol), quarterLength=quarter_length_duration)
-                    stream.append(m21_event)
+        try:
+            for i, symbol in enumerate(melody):
+                # Handle case in which we have a note/rest
+                if symbol != "_" or i + 1 == len(melody):
+                    # Ensure we're dealing with note/rest beyond the first note/rest
+                    if start_symbol is not None:
+                        quarter_length_duration = step_duration * step_counter  # 0.25 * 4 = 1
+                        # Handle rest
+                        if start_symbol == "r":
+                            m21_event = m21.note.Rest(quarterLength=quarter_length_duration)
+                        # Handle note
+                        else:
+                            m21_event = m21.note.Note(int(start_symbol), quarterLength=quarter_length_duration)
+                        stream.append(m21_event)
 
-                    # Reset the step counter
-                    step_counter = 1
+                        # Reset the step counter
+                        step_counter = 1
 
-                start_symbol = symbol
-            # Handle case in which we have a prolongation sign "_"
-            else:
-                step_counter += 1
+                    start_symbol = symbol
+                # Handle case in which we have a prolongation sign "_"
+                else:
+                    step_counter += 1
+        except Exception as e:
+            print(f'Exception raised: {e}')
         # Write the m21 stream to a midi file
         stream.write(format, file_name)
 
